dee759427fb3a746c0ab351a769f206d395d1abd
kdm326 <108142704+kdm326@users.noreply.github.com>
2022-09-22 01:14:18 -0400

    Initial Commit

---
+++ Shared/SearchDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchDemoApp.swift
+//  Shared
+//
+//  Created by Kevin McKenney on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct SearchDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
